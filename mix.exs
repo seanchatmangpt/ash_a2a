5456714e087020c6633c1446d936526bf73bcd09
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-09-24 21:54:59 -0700

    bump: release v26.9.24

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule AshA2A.MixProject do
   def project do
     [
       app: :ash_a2a,
-      version: "26.9.22",
+      version: "26.9.24",
       source_url: "https://github.com/seanchatmangpt/ash_a2a",
       homepage_url: "https://hexdocs.pm/ash_a2a/",
       elixir: "~> 1.19",
